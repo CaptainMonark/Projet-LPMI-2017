b95887c3748638d5b4687ecda09cf553cccf82c4
Alexvig <alexan.vig@gmail.com>
2018-02-02 18:18:20 +0100

    erreur

--- src/connexion.php
+++ src/connexion.php
@@ -1,6 +1,4 @@
 <?php
-    /*$link = new mysqli("localhost","root","coccinelle21!","shop");
     if(mysqli_connect_errno()){
         printf('Echec de la connexion: %s\n"', mysqli_connect_error());
-    }*/
 ?>
