6fdf458ee7be8996a6898ed6da75ce9c7724fab7
CaptainMonark <mrponch@live.fr>
2018-02-02 18:56:07 +0100

    utf8

--- src/connexion.php
+++ src/connexion.php
@@ -1,6 +1,9 @@
 <?php
-    $link = new mysqli("localhost","root","","baselpmi");
+    $link = new mysqli("localhost","root","","projet_lpmi");
     if(mysqli_connect_errno()){
         printf('Echec de la connexion: %s\n"', mysqli_connect_error());
     }
+    if (!$link->set_charset("utf8")) {
+        exit();
+    } 
 ?>
--- src/www/inscription.php
+++ src/www/inscription.php
@@ -1,8 +1,5 @@
 <?php
-  $link = new mysqli("localhost","root","","baselpmi");
-  if(mysqli_connect_errno()){
-      printf('Echec de la connexion: %s\n"', mysqli_connect_error());
-  }
+include('src/connexion.php');
 $result = $link->query("SELECT * FROM utilisateur WHERE mail = 'f.l@gmail.com'");
 $mail = $result->fetch_object();
 echo $mail->nom;
