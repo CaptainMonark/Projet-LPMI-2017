8cd684be4d67fe5c7a4002aea081ebdad75e1357
Alexvig <alexan.vig@gmail.com>
2018-02-02 18:25:30 +0100

    ignore connexion

--- src/connexion.php
+++ src/connexion.php
@@ -1,8 +1,4 @@
 <?php
-<<<<<<< HEAD
-=======
-    $link = new mysqli("localhost","root","","projet_lpmi");
->>>>>>> ac733a60da19a057f02e608f68b9adb8e16eacdb
     $link = new mysqli("localhost","root","","baselpmi");
     if(mysqli_connect_errno()){
         printf('Echec de la connexion: %s\n"', mysqli_connect_error());
