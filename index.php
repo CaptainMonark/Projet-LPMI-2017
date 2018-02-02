42917bfdd07514dbc26a60d6beb81fc5496a660f
CaptainMonark <mrponch@live.fr>
2018-02-02 15:54:38 +0100

    Ajout accueil

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 		<link rel="stylesheet" media="screen" type="text/css" title="design" href="src/css/style.css" />
 		<link rel="stylesheet" media="screen" type="text/css" title="design" href="src/css/header.css" />
 		<link rel="stylesheet" media="screen" type="text/css" title="design" href="src/css/footer.css" />
+		<link rel="stylesheet" media="screen" type="text/css" title="design" href="src/css/accueil.css" />
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	</head>
---
+++ src/www/accueil.php
@@ -0,0 +1,49 @@
+<div class="opacity">
+</div>
+
+<div class="accueil">
+<!-- Slideshow container -->
+<div class="slider">
+<ul>
+  <li id="no-js-slider-1" class="slide">
+    <div class="fondS1"></div>
+    <div class="slide1">
+        <div class="presentation">
+            <p>Présentation : </p>
+            <p>Sandrine Lanquetin, Docteur en Informatique de l'Université de Bourgogne,</p>
+            <p>Maître de conférences en informatique à l'université de Dijon</p>
+            <ul >
+                <li class="listePres">
+                    Enseignement à l'UFR des sciences et techniques de Dijon
+                </li>
+                <li class="listePres"> 
+                    Recherche en modélisation géométrique au laboratoire d'Electronique et d'informatique de l'Image (LE2I) à Dijon
+                </li>
+            </ul>
+        </div>
+    </div>
+    <a class="prev" href="#no-js-slider-1">prev</a>
+    <a class="next" href="#no-js-slider-3">next</a>  
+  </li>
+  <li id="no-js-slider-2" class="slide">
+    <img class="img_fond_slider" src="src/assets/images/fond_accueil2.jpg">
+    <img class="img_slider" src="src/assets/images/fond_accueil2.jpg">
+    <a class="prev" href="#no-js-slider-1">prev</a>
+    <a class="next" href="#no-js-slider-3">next</a>           
+  </li>
+  <li id="no-js-slider-3" class="slide">
+    <img class="img_fond_slider" src="src/assets/images/fond_accueil3.jpg">
+    <img class="img_slider" src="src/assets/images/fond_accueil3.jpg">
+    <a class="prev" href="#no-js-slider-2">prev</a>
+    <a class="next" href="#no-js-slider-4">next</a>           
+  </li>
+  <li id="no-js-slider-3" class="slide">
+    <img class="img_fond_slider" src="src/assets/images/fond_accueil4.jpg">
+    <img class="img_slider" src="src/assets/images/fond_accueil4.jpg">
+    <a class="prev" href="#no-js-slider-3">prev</a>
+    <a class="next" href="#no-js-slider-1">next</a>           
+  </li>
+</ul>
+</div>
+        
+</div>
