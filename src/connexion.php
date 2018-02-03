884bccf6d96e9431e3a8d7e1ace0574febfd44a8
Alexvig <alexan.vig@gmail.com>
2018-02-03 03:14:21 +0100

    login

--- src/connexion.php
+++ src/connexion.php
@@ -1,5 +1,5 @@
 <?php
-    $link = new mysqli("localhost","root","","projet_lpmi");
+    $link = new mysqli("localhost","root","","baselpmi");
     if(mysqli_connect_errno()){
         printf('Echec de la connexion: %s\n"', mysqli_connect_error());
     }
