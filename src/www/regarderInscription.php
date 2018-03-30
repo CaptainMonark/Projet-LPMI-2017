5d86a720f9d1978d9d91fea537ddd538525b82d9
CaptainMonark <mrponch@live.fr>
2018-03-30 06:18:43 +0200

    fix 9

--- src/www/regarderInscription.php
+++ src/www/regarderInscription.php
@@ -1,16 +1,17 @@
 <?php
 include('src/connexion.php');
 $tableau = "";
-    $query = "select u.nom as nom, u.prenom as prenom from utilisateur u JOIN fichieretu fe ON fe.idU = u.id where projet = '".$_GET['projet']."'";
+    $query = "select u.nom as nom, u.prenom as prenom, fe.nom as titre from utilisateur u JOIN fichieretu fe ON fe.idU = u.id where projet = '".$_GET['projet']."'";
     if($result = mysqli_query($link,$query)){
         $tableau = ' 
         <table id="t01">
             <tr> 
                 <th>Nom</th> 
-                <th>Prenom</th> 
+                <th>Prenom</th>
+                <th>Titre du fichier</th>
             </tr> ';
         while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
-            $tableau = $tableau.'<tr> <td>'.$row['nom'].'</td> <td>'.$row['prenom'].'</td></tr> ';
+            $tableau = $tableau.'<tr> <td>'.$row['nom'].'</td> <td>'.$row['prenom'].'</td></td> <td>'.$row['titre'].'</td> </tr> ';
         }
         $tableau = $tableau.'</table>';
         mysqli_free_result($result);
